179bd993af3897d761359683efdc4886870d9f2d
ghettoDdOS <pen.egor2002@gmail.com>
2021-06-23 23:10:40 +0300

    deploy

--- hackaton/settings.py
+++ hackaton/settings.py
@@ -19,9 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = "django-insecure-0!*c32(h(=4&xxk^y#@9(=j2ddvvgq-bo%z8x04gvd*vkef)2_"
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = ["ghettoddos.pythonanywhere.com"]
 
 
 # Application definition
