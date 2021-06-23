89c978c5f8715bd6faa980c8577d4050a054beab
ghettoDdOS <pen.egor2002@gmail.com>
2021-06-23 23:09:27 +0300

    deploy

--- hackaton/settings.py
+++ hackaton/settings.py
@@ -19,12 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0!*c32(h(=4&xxk^y#@9(=j2ddvvgq-bo%z8x04gvd*vkef)2_"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
--- hackaton/wsgi.py
+++ hackaton/wsgi.py
@@ -12,5 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackaton.settings')
+os.environ.setdefault('DEBUG', 'FALSE')
+os.environ.setdefault('ALLOWED_HOSTS', 'ghettoddos.pythonanywhere.com')
+os.environ.setdefault('SECRET_KEY', 'django-insecure-0!*c32(h(=4&xxk^y#@9(=j2ddvvgq-bo%z8x04gvd*vkef)2_')
 
 application = get_wsgi_application()
